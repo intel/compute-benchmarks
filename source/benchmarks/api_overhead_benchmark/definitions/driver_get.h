58b33adbd495f1f12194e8e5e9a34729e89c566b
Barigou, Youcef <youcef.barigou@intel.com>
2022-07-26 14:06:13 -0700

    Adding DriverGet micro-benchmark to measure api overhead call for both driver count and driver get
    Signed-off-by: Barigou, Youcef <youcef.barigou@intel.com>

---
+++ source/benchmarks/api_overhead_benchmark/definitions/driver_get.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#pragma once
+
+#include "framework/argument/basic_argument.h"
+#include "framework/test_case/test_case.h"
+
+struct DriverGetArguments : TestCaseArgumentContainer {
+    BooleanArgument getDriverCount;
+
+    DriverGetArguments()
+        : getDriverCount(*this, "getDriverCount", "Whether to measure driver count or driver get") {}
+};
+
+struct DriverGet : TestCase<DriverGetArguments> {
+    using TestCase<DriverGetArguments>::TestCase;
+
+    std::string getTestCaseName() const override {
+        return "DriverGet";
+    }
+
+    std::string getHelp() const override {
+        return "measures time spent in driver get call for CPU.";
+    }
+};
