aa853d393a2ad537b73b77d01161a4baba72c5b9
Barigou, Youcef <youcef.barigou@intel.com>
2022-08-03 17:42:23 +0000

    Adding DriverGet overhead microbenchmark
    Related-To: LOCI-3280
    
    Signed-off-by: Barigou, Youcef <youcef.barigou@intel.com>

---
+++ source/benchmarks/api_overhead_benchmark/gtest/driver_get.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "definitions/driver_get.h"
+
+#include "framework/test_case/register_test_case.h"
+
+#include <gtest/gtest.h>
+
+static const inline RegisterTestCase<DriverGet> registerTestCase{};
+
+class DriverGetTest : public ::testing::TestWithParam<size_t> {
+};
+
+TEST_P(DriverGetTest, Test) {
+    DriverGetArguments args{};
+    args.api = Api::L0;
+    args.getDriverCount = GetParam();
+    DriverGet test;
+    test.run(args);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    DriverGetTest,
+    DriverGetTest,
+    ::testing::Values(0, 1, 4));
---
+++ source/benchmarks/api_overhead_benchmark/implementations/l0/driver_get_l0.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "framework/l0/levelzero.h"
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/timer.h"
+
+#include "definitions/driver_get.h"
+
+#include <gtest/gtest.h>
+
+static TestResult run(const DriverGetArguments &arguments, Statistics &statistics) {
+    // Setup
+    QueueProperties queueProperties = QueueProperties::create().disable();
+    LevelZero levelzero(queueProperties);
+    Timer timer;
+
+    // Warmup
+    uint32_t driverCount = 0;
+    if (arguments.getDriverCount) {
+        ASSERT_ZE_RESULT_SUCCESS(zeDriverGet(&driverCount, nullptr));
+    } else {
+        ASSERT_ZE_RESULT_SUCCESS(zeDriverGet(&driverCount, nullptr));
+        std::vector<ze_driver_handle_t> drivers(driverCount);
+        ASSERT_ZE_RESULT_SUCCESS(zeDriverGet(&driverCount, drivers.data()));
+    }
+    driverCount = 0;
+
+    // Benchmark
+    for (auto j = 0u; j < arguments.iterations; j++) {
+        if (arguments.getDriverCount) {
+            timer.measureStart();
+            ASSERT_ZE_RESULT_SUCCESS(zeDriverGet(&driverCount, nullptr));
+            timer.measureEnd();
+        } else {
+            ASSERT_ZE_RESULT_SUCCESS(zeDriverGet(&driverCount, nullptr));
+            EXPECT_NE(0, driverCount);
+            std::vector<ze_driver_handle_t> drivers(driverCount);
+            timer.measureStart();
+            ASSERT_ZE_RESULT_SUCCESS(zeDriverGet(&driverCount, drivers.data()));
+            timer.measureEnd();
+        }
+
+        statistics.pushValue(timer.get(), MeasurementUnit::Microseconds, MeasurementType::Cpu);
+
+        driverCount = 0;
+    }
+    return TestResult::Success;
+}
+
+static RegisterTestCaseImplementation<DriverGet> registerTestCase(run, Api::L0);
