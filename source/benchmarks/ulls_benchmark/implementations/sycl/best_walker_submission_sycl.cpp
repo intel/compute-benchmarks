4e063455082aa608787f35ab9aa7a3a4749e55f4
Lukasz Jobczyk <lukasz.jobczyk@intel.com>
2023-02-24 10:34:07 +0000

    Add best walker submission SYCL benchmark
    Signed-off-by: Lukasz Jobczyk <lukasz.jobczyk@intel.com>

---
+++ source/benchmarks/ulls_benchmark/implementations/sycl/best_walker_submission_sycl.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "framework/sycl/sycl.h"
+#include "framework/sycl/utility/usm_helper.h"
+#include "framework/test_case/register_test_case.h"
+#include "framework/utility/timer.h"
+
+#include "definitions/best_walker_submission.h"
+
+static TestResult run(const BestWalkerSubmissionArguments &arguments, Statistics &statistics) {
+    MeasurementFields typeSelector(MeasurementUnit::Microseconds, MeasurementType::Cpu);
+
+    if (isNoopRun()) {
+        statistics.pushUnitAndType(typeSelector.getUnit(), typeSelector.getType());
+        return TestResult::Nooped;
+    }
+
+    // Setup
+    Sycl sycl{sycl::gpu_selector{}};
+    Timer timer;
+    auto size = 1u;
+
+    // Create buffer
+    volatile auto buffer = sycl::malloc_host<uint64_t>(size, sycl.queue);
+
+    // Create kernel
+    const auto writeOne = [=](auto i) {
+        buffer[i] = 1u;
+    };
+
+    // Warmup
+    sycl.queue.parallel_for(size, writeOne).wait();
+
+    // Benchmark
+    for (auto i = 0u; i < arguments.iterations; i++) {
+        *buffer = 0u;
+
+        timer.measureStart();
+
+        sycl.queue.parallel_for(size, writeOne);
+        while (*buffer != 1u) {
+        }
+
+        timer.measureEnd();
+
+        sycl.queue.wait();
+        statistics.pushValue(timer.get(), typeSelector.getUnit(), typeSelector.getType());
+    }
+
+    // Cleanup
+    sycl::free(buffer, sycl.queue);
+
+    return TestResult::Success;
+}
+
+static RegisterTestCaseImplementation<BestWalkerSubmission> registerTestCase(run, Api::SYCL);
