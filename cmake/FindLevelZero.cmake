823a8f7eb71e99225d5d15dd408ef9cbba6aee83
Lukasz Jobczyk <lukasz.jobczyk@intel.com>
2023-03-31 08:01:51 +0000

    Fix FindLevelZero.cmake
    Signed-off-by: Lukasz Jobczyk <lukasz.jobczyk@intel.com>

--- cmake/FindLevelZero.cmake
+++ cmake/FindLevelZero.cmake
@@ -1,22 +1,24 @@
 #
-# Copyright (C) 2022 Intel Corporation
+# Copyright (C) 2022-2023 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
 
 include(FindPackageHandleStandardArgs)
 
+SET(LevelZero_SEARCH_PATHS
+  /usr
+)
+
 find_path(LevelZero_INCLUDE_DIR
   NAMES level_zero/ze_api.h
-  PATHS
-    ${L0_ROOT}
+  PATHS ${LevelZero_SEARCH_PATHS}
   PATH_SUFFIXES "include"
 )
 
 find_library(LevelZero_LIBRARY
-  NAMES ze_loader ze_loader32 ze_loader64
-  PATHS
-    ${L0_ROOT}
+  NAMES ze_loader
+  PATHS ${LevelZero_SEARCH_PATHS}
   PATH_SUFFIXES "lib" "lib/"
 )
 
@@ -24,11 +26,9 @@ find_package_handle_standard_args(LevelZero
   REQUIRED_VARS
     LevelZero_INCLUDE_DIR
     LevelZero_LIBRARY
-  HANDLE_COMPONENTS
 )
-mark_as_advanced(LevelZero_LIBRARY LevelZero_INCLUDE_DIR)
 
 if(LevelZero_FOUND)
-    list(APPEND LevelZero_LIBRARIES ${LevelZero_LIBRARY} ${CMAKE_DL_LIBS})
-    list(APPEND LevelZero_INCLUDE_DIRS ${LevelZero_INCLUDE_DIR})
+  list(APPEND LevelZero_LIBRARIES ${LevelZero_LIBRARY} ${CMAKE_DL_LIBS})
+  list(APPEND LevelZero_INCLUDE_DIRS ${LevelZero_INCLUDE_DIR})
 endif()
