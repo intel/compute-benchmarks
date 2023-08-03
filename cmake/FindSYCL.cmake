57d63ab3ea2859d3908c70e739a0680b7fe49e53
Ben Ashbaugh <ben.ashbaugh@intel.com>
2023-08-03 00:01:43 +0000

    refactor FindSYCL.cmake to work with GitHub SYCL compilers
    Signed-off-by: Ben Ashbaugh <ben.ashbaugh@intel.com>

--- cmake/FindSYCL.cmake
+++ cmake/FindSYCL.cmake
@@ -4,42 +4,40 @@
 # SPDX-License-Identifier: MIT
 #
 
-set(SYCL_FOUND FALSE CACHE BOOL "SYCL Found")
-set(INTEL_COMPILER_ROOT $ENV{CMPLR_ROOT})
+set(SYCL_FOUND FALSE)
 
-if(NOT INTEL_COMPILER_ROOT)
-    message(WARNING "FindSYCL: Intel oneAPI environment not set. Ensure that Intel oneAPI is installed and use the setvars script.")
-    return()
-endif()
-
-set(_CXX_COMPILER ${CMAKE_CXX_COMPILER})
-set(_CXX_COMPILER_ID ${CMAKE_CXX_COMPILER_ID})
-set(_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-set(CMAKE_CXX_COMPILER "icpx")
-set(CMAKE_CXX_COMPILER_ID "IntelLLVM")
-
-set(SYCL_INCLUDE_DIR ${INTEL_COMPILER_ROOT}/linux/include)
-set(SYCL_LIBRARY_DIR ${INTEL_COMPILER_ROOT}/linux/lib)
-
-find_package(IntelSYCL) 
-if(NOT ${IntelSYCL_FOUND})
-    message(WARNING "Intel SYCL installation not found")
-else()
-    message(STATUS "Intel SYCL installation found")
-    string(REGEX REPLACE " $" "" SYCL_FLAGS "${SYCL_FLAGS}")
-    
-    if(BUILD_SYCL_WITH_CUDA)
-        set(SYCL_FLAGS "${SYCL_FLAGS} -fsycl-targets=nvptx64-nvidia-cuda,spir64")
+if(NOT SYCL_COMPILER_ROOT)
+    set(SYCL_COMPILER_ROOT $ENV{CMPLR_ROOT})
+    if(NOT SYCL_COMPILER_ROOT)
+        message(WARNING "FindSYCL: Intel oneAPI environment not set. Ensure that Intel oneAPI is installed and use the setvars script.")
+        return()
     endif()
-
-    set(SYCL_FOUND TRUE)
-    set(SYCL_CFLAGS "${SYCL_FLAGS}" CACHE STRING "SYCL Compiler Flags")
-    set(SYCL_LFLAGS "${SYCL_CFLAGS} -lsycl" CACHE STRING "SYCL Linker Flags")
-    set(SYCL_INCLUDE_DIR "${SYCL_INCLUDE_DIR}" CACHE PATH "SYCL Inlcude Directory")
-
-    set(SYCL_PROXY "${CMAKE_SOURCE_DIR}/scripts/icpx-proxy.sh" CACHE STRING "SYCL Compiler Proxy")
+    set(SYCL_COMPILER_ROOT ${SYCL_COMPILER_ROOT}/linux)
 endif()
 
-set(CMAKE_CXX_COMPILER ${_CXX_COMPILER})
-set(CMAKE_CXX_COMPILER_ID ${_CXX_COMPILER_ID})
-set(CMAKE_CXX_FLAGS ${_CXX_FLAGS})
+if(NOT SYCL_COMPILER)
+    if (WIN32)
+        set(SYCL_COMPILER icx)
+    else()
+        find_program(HAS_ICPX "icpx" NO_CACHE)
+        if (HAS_ICPX)
+            set(SYCL_COMPILER icpx)
+        else()
+            set(SYCL_COMPILER clang++)
+        endif()
+    endif()
+endif()
+
+set(SYCL_FLAGS "-fsycl")
+if(BUILD_SYCL_WITH_CUDA)
+    set(SYCL_FLAGS "${SYCL_FLAGS} -fsycl-targets=nvptx64-nvidia-cuda,spir64")
+endif()
+
+set(SYCL_FOUND TRUE)
+set(SYCL_CFLAGS "${SYCL_FLAGS}" CACHE STRING "SYCL Compiler Flags")
+set(SYCL_LFLAGS "${SYCL_CFLAGS} -lsycl" CACHE STRING "SYCL Linker Flags")
+set(SYCL_INCLUDE_DIR "${SYCL_COMPILER_ROOT}/include" CACHE PATH "SYCL Include Directory")
+set(SYCL_LIBRARY_DIR "${SYCL_COMPILER_ROOT}/lib" CACHE PATH "SYCL Library Directory")
+
+configure_file(${PROJECT_SOURCE_DIR}/scripts/sycl-proxy.sh.in "${CMAKE_CURRENT_BINARY_DIR}/sycl-proxy.sh" @ONLY)
+set(SYCL_PROXY "${CMAKE_CURRENT_BINARY_DIR}/sycl-proxy.sh" CACHE STRING "SYCL Compiler Proxy")
