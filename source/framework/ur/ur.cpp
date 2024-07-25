55a592212ab5ea4eab81abb2b65b0647e0f31505
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-07-25 21:46:17 +0000

    Add Unified Runtime backend and implement submit_kernel for ur

---
+++ source/framework/ur/ur.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include "ur.h"
+
+#include "error.h"
+
+#include <stdexcept>
+#include <vector>
+
+UrState::UrState() {
+    ur_device_init_flags_t device_flags = 0;
+    EXPECT_UR_RESULT_SUCCESS(urLoaderInit(device_flags, nullptr));
+
+    uint32_t adapter_count = 0;
+    EXPECT_UR_RESULT_SUCCESS(urAdapterGet(0, nullptr, &adapter_count));
+
+    if (adapter_count != 1) {
+        FATAL_ERROR("Detected " + std::to_string(adapter_count) +
+                    " adapters, expected 1.");
+    }
+
+    EXPECT_UR_RESULT_SUCCESS(urAdapterGet(adapter_count, &adapter, nullptr));
+
+    uint32_t platform_count = 0;
+    EXPECT_UR_RESULT_SUCCESS(urPlatformGet(&adapter, adapter_count, 0, nullptr, &platform_count));
+
+    std::vector<ur_platform_handle_t> platforms(platform_count);
+    EXPECT_UR_RESULT_SUCCESS(urPlatformGet(&adapter, adapter_count, platform_count, platforms.data(),
+                                           nullptr));
+
+    if (Configuration::get().urPlatformIndex >= platform_count) {
+        FATAL_ERROR("Invalid UR platform index. platformIndex=",
+                    Configuration::get().urPlatformIndex, " platformCount=",
+                    platform_count);
+    }
+    platform = platforms[Configuration::get().urPlatformIndex];
+
+    uint32_t device_count = 0;
+    EXPECT_UR_RESULT_SUCCESS(urDeviceGet(platform, UR_DEVICE_TYPE_ALL, 0, nullptr, &device_count));
+
+    std::vector<ur_device_handle_t> devices(device_count);
+    EXPECT_UR_RESULT_SUCCESS(urDeviceGet(platform, UR_DEVICE_TYPE_ALL, device_count, devices.data(),
+                                         nullptr));
+
+    if (Configuration::get().urDeviceIndex >= device_count) {
+        FATAL_ERROR("Invalid UR device index. deviceIndex=",
+                    Configuration::get().urDeviceIndex, " deviceCount=",
+                    device_count);
+    }
+    device = devices[Configuration::get().urDeviceIndex];
+
+    for (auto &device : devices) {
+        if (device != this->device) {
+            urDeviceRelease(device);
+        }
+    }
+
+    EXPECT_UR_RESULT_SUCCESS(urContextCreate(1, &device, nullptr, &context));
+}
+
+UrState::~UrState() {
+    if (device) {
+        urDeviceRelease(device);
+    }
+    if (context) {
+        urContextRelease(context);
+    }
+    if (adapter) {
+        urAdapterRelease(adapter);
+    }
+    urLoaderTearDown();
+}
