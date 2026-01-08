5d4f4ad6712d34214359661b1bc067a0f26ea6de
Alicja Lukaszewicz <alicja.lukaszewicz@intel.com>
2026-01-08 13:41:28 +0000

    refactor: simplify kernel file copying
    Related-To: NEO-16306
    
    Signed-off-by: Alicja Lukaszewicz <alicja.lukaszewicz@intel.com>

--- cmake/CopyKernels.cmake
+++ cmake/CopyKernels.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2025 Intel Corporation
+# Copyright (C) 2025-2026 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 #
@@ -7,19 +7,19 @@
 set(KERNELS_SOURCE_DIR "${CMAKE_SOURCE_DIR}/source/kernels")
 set(KERNELS_OUTPUT_DIR "${OUTPUT_DIR}")
 
-file(GLOB_RECURSE KERNEL_CL_FILES CONFIGURE_DEPENDS
-    "${KERNELS_SOURCE_DIR}/*.cl"
-)
+include(${CMAKE_MODULE_PATH}/${BRANCH_TYPE}/CopyKernelsAdditional.cmake OPTIONAL)
 
-file(GLOB_RECURSE KERNEL_SPV_FILES CONFIGURE_DEPENDS
-    "${KERNELS_SOURCE_DIR}/*.spv"
-)
+list(APPEND KERNEL_EXTENSIONS ".cl" ".spv")
 
-function(copy_kernel_files)
-    file(MAKE_DIRECTORY "${KERNELS_OUTPUT_DIR}")
-    foreach(KERNEL_FILE ${ARGN})
-        configure_file("${KERNEL_FILE}" "${KERNELS_OUTPUT_DIR}" COPYONLY)
-    endforeach()
-endfunction()
+set(KERNEL_FILES "")
+foreach(EXTENSION ${KERNEL_EXTENSIONS})
+    file(GLOB_RECURSE EXTENSION_FILES CONFIGURE_DEPENDS
+        "${KERNELS_SOURCE_DIR}/*${EXTENSION}"
+    )
+    list(APPEND KERNEL_FILES ${EXTENSION_FILES})
+endforeach()
 
-copy_kernel_files(${KERNEL_CL_FILES} ${KERNEL_SPV_FILES})
+file(MAKE_DIRECTORY "${KERNELS_OUTPUT_DIR}")
+foreach(KERNEL_FILE ${KERNEL_FILES})
+    configure_file("${KERNEL_FILE}" "${KERNELS_OUTPUT_DIR}" COPYONLY)
+endforeach()
